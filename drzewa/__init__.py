f4099369bdbab3b35e308b6ca564e1b7e7988b91
lszydlik <xxstszydlik@gmail.com>
2024-04-17 00:29:33 +0200

    add find node and delete node

--- drzewa/__init__.py
+++ drzewa/__init__.py
@@ -3,11 +3,14 @@ from bst_tree import Tree_BST
 
 if __name__ == "__main__":
     tree_bst = Tree_BST()
-    tree_bst.insert(2)
-    tree_bst.insert(1)
-    tree_bst.insert(3)
-    tree_bst.insert(1)
-    tree_bst.insert(7)
-    tree_bst.insert(3)
+    tree_bst.insert(9)
+    tree_bst.insert(15)
+    tree_bst.insert(10)
+    tree_bst.insert(18)
 
     pass
+
+    tree_bst.delete_node(10)
+
+    pass
+
