156873e152d4e722cefac0eeafef8a40b52df9ea
Marek Dzieciol <mdziecio@loch-75.lochy.elka.pw.edu.pl>
2024-04-18 15:17:01 +0200

    Poprawka avl

--- drzewa/avl_tree.py
+++ drzewa/avl_tree.py
@@ -11,78 +11,130 @@ class AVLTree:
     def __init__(self):
         self.root = None
 
-    def right_rotation(self, node):
-        parent = node.parent
-        child = node.right_child
-        child2 = child.left_child
-        if child2 is None:
-            child2 = child.right_child
+    def ll_rot(self, y, z):
+        a = z.left_child
+        b = z.right_child
+        c = y.right_child
+        p = y.parent
 
-        node.parent = child
-        child.left_child = node
-        child.right_child = child2
-        child.parent = parent
-        node.right_child = None
-
-        if parent is not None:
-            if node is parent.left_child:
-                parent.left_child = child
+        if p is not None:
+            if p.left_child is y:
+                p.left_child = z
             else:
-                parent.right_child = child
-        else:
-            self.root = child
+                p.right_child = z
+            z.parent = p
+        
+        z.left_child = a
+        a.parent = z
+        z.right_child = y
+        y.parent = z
+        y.left_child = b
+        b.parent = y
+        y.right_child = c
+        c.parent = y
 
-        child2.bf = 0
-        if node.left_child is not None:
-            node.bf = -1
-            child.bf = -1
-        else:
-            node.bf = 0
-            child.bf = 0
+        z.bf = 0
+        y.bf = 0
+    
+    def rr_rot(self, y, z):
+        a = z.left_child
+        b = z.right_child
+        c = y.left_child
+        p = y.parent
 
-    def left_rotation(self, node):
-        parent = node.parent
-        child = node.left_child
-        child2 = child.left_child
-        if child2 is None:
-            child2 = child.right_child
-
-        node.parent = child
-        child.right_child = node
-        child.left_child = child2
-        child.parent = parent
-        node.left_child = None
-
-        if parent is not None:
-            if node is parent.left_child:
-                parent.left_child = child
+        if p is not None:
+            if p.left_child is y:
+                p.left_child = z
             else:
-                parent.right_child = child
-        else:
-            self.root = child
+                p.right_child = z
+            z.parent = p
+        
+        z.left_child = y
+        y.parent = z
+        y.right_child = a
+        a.parent = y
 
-        child2.bf = 0
-        if node.right_child is not None:
-            node.bf = 1
-            child.bf = 1
-        else:
-            node.bf = 0
-            child.bf = 0
+        z.bf = 0
+        y.bf = 0
+    
+    def lr_rot(self, y, z, t):
+        b = t.left_child
+        c = t.right_child
+        p = y.parent
 
-    def balance(self, node):
-        if (node.bf > 1):
-            self.right_rotation(node)
-        elif (node.bf < -1):
-            self.left_rotation(node)
+        if p is not None:
+            if p.left_child is y:
+                p.left_child = t
+            else:
+                p.right_child = t
+            z.parent = t
+        
+        t.left_child = z
+        z.parent = t
+        z.right_child = b
+        b.parent = z
+        t.right_child = y
+        y.parent = t
+        y.left_child = c
+        c.parent = y
+
+        if (t.bf == -1):
+            z.bf = 1
+            y.bf = 0
+        else:
+            z.bf = 0
+            y.bf = -1
+        t.bf = 0
+        
+
+
+    def rl_rot(self, y, z, t):
+        b = t.left_child
+        c = t.right_child
+        p = y.parent
+
+        if p is not None:
+            if p.left_child is y:
+                p.left_child = t
+            else:
+                p.right_child = t
+            z.parent = t
+        
+        t.left_child = y
+        y.parent = t
+        y.right_child = b
+        b.parent = y
+        t.right_child = z
+        z.parent = t
+        z.left_child = c
+        c.parent = z
+
+        if (t.bf == -1):
+            z.bf = 1
+            y.bf = 0
+        else:
+            z.bf = 0
+            y.bf = -1
+        t.bf = 0
 
     def update_parent_bf(self, parent, child_node):
         if parent.left_child is child_node:
-            parent.bf -= 1
-        elif parent.right_child is child_node:
             parent.bf += 1
+        elif parent.right_child is child_node:
+            parent.bf -= 1
 
-        if (parent.bf < -1 or parent.bf > 1):
-            self.balance(parent)
+        if parent.parent is not None:
+            self.update_parent_bf(parent.parent, parent)
+
+        if (parent.bf == 2 and child_node.bf == 1):
+            self.ll_rot(parent, child_node)
+        elif (parent.bf == -2 and child_node.bf == -1):
+            self.rr_rot(parent, child_node)
+        elif (parent.bf == 2 and child_node.bf == -1):
+            self.lr_rot(parent, child_node, child_node.right)
+        elif (parent.bf == -2 and child_node.bf == 1):
+            self.rl_rot(parent, child_node, child_node.right)
+
 
     def search(self, value):
         curr_node = self.root
@@ -108,9 +160,7 @@ class AVLTree:
                 else:
                     new_node = AVLNode(value, curr_node)
                     curr_node.left_child = new_node
-                    curr_node.bf -= 1
-                    if (curr_node.bf == -1 and curr_node.parent is not None):
-                        self.update_parent_bf(curr_node.parent, curr_node)
+                    self.update_parent_bf(curr_node, new_node)
                     break
             else:
                 if curr_node.right_child is not None:
@@ -118,9 +168,7 @@ class AVLTree:
                 else:
                     new_node = AVLNode(value, curr_node)
                     curr_node.right_child = new_node
-                    curr_node.bf += 1
-                    if (curr_node.bf == 1 and curr_node.parent is not None):
-                        self.update_parent_bf(curr_node.parent, curr_node)
+                    self.update_parent_bf(curr_node, new_node)
                     break
 
     def inorder(self, node, depth=0):
